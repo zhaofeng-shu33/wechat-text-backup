71077859a3afbb487f310d9e27d87ad253fbae16
zhaofeng-shu33 <616545598@qq.com>
2020-04-07 17:03:23 +0800

    fix warning

--- decrypt.cpp
+++ decrypt.cpp
@@ -94,7 +94,7 @@ int Decryptdb()
     int offset = 16;
     while (pTemp < pDbBuffer + nFileSize)
     {
-        printf("解密数据页:%d/%d \n", nPage, nFileSize / DEFAULT_PAGESIZE);
+        printf("解密数据页:%d/%d \n", nPage, int(nFileSize / DEFAULT_PAGESIZE));
  
 #ifndef NO_USE_HMAC_SHA1
         unsigned char hash_mac[HMAC_SHA1_SIZE] = { 0 };
