85457b5020574f04a5b7995f6eef04e09bd5d96e
zhaofeng-shu33 <616545598@qq.com>
2020-04-07 17:46:31 +0800

    update win upload code

--- win-upload-script.sh
+++ win-upload-script.sh
@@ -2,5 +2,6 @@
 user=~/Documents/"WeChat Files"/zhaofeng_shu33/Msg
 mkdir -p db/Multi
 find "$user" -name "*.db" | xargs -d '\n' cp -t db
+find "$user"/Multi/ -name "*.db" | xargs -d '\n' cp -t db/Multi
 tar -czvf archive-$(date +%F).tar.gz db
 
