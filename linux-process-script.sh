17a1761e9361cff692bd7388d3194ebe1af95cc3
zhaofeng-shu33 <616545598@qq.com>
2020-04-23 18:41:38 +0800

    add linux pre-processing

--- linux-process-script.sh
+++ linux-process-script.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+if [ -f ".upload.sh" ]; then
+    bash .upload.sh
+fi
 set -e -x
 tar -xzvf archive-$(date +%F).tar.gz
+
 mkdir -p dec_db/Multi
 
 mapfile -t db_file_list < <(find db/ -name "*.db")
 for i in "${db_file_list[@]}"; do
     ./decrypt $i
-    mv $i.dec.db dec_$i.dec.db
+    mv $i.dec.db dec_$i.dec.db &
 done
 
 
