58e4e28d17df31e381075485ab201c9c69002ab0
zhaofeng-shu33 <616545598@qq.com>
2020-04-23 18:37:08 +0800

    add upload script post-processing

--- win-upload-script.sh
+++ win-upload-script.sh
@@ -4,4 +4,6 @@ mkdir -p db/Multi
 find "$user" -name "*.db" | xargs -d '\n' cp -t db
 find "$user"/Multi/ -name "*.db" | xargs -d '\n' cp -t db/Multi
 tar -czvf archive-$(date +%F).tar.gz db
-
+if [ -f ".upload.sh" ]; then
+    bash .upload.sh
+fi
