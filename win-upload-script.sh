19609ec5c460c7fd4bcc563040dcc00bbfc880a6
zhaofeng-shu33 <616545598@qq.com>
2020-08-08 20:49:31 +0800

    fix win upload script

--- win-upload-script.sh
+++ win-upload-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 user=~/Documents/"WeChat Files"/zhaofeng_shu33/Msg
 mkdir -p db/Multi
-find "$user" -name "*.db" | xargs -d '\n' cp -t db
-find "$user"/Multi/ -name "*.db" | xargs -d '\n' cp -t db/Multi
+cp "$user"/MicroMsg.db.dec.db db/
+cp "$user"/Multi/MSG0.db.dec.db db/Multi/
 tar -czvf archive-$(date +%F).tar.gz db
 if [ -f ".upload.sh" ]; then
     bash .upload.sh
