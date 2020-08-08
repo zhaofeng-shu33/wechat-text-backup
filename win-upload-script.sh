9526797b300b2fba93ce85871b425a91f692925a
zhaofeng-shu33 <616545598@qq.com>
2020-08-08 20:52:44 +0800

    change pack script

--- win-upload-script.sh
+++ win-upload-script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 user=~/Documents/"WeChat Files"/zhaofeng_shu33/Msg
+rm -rf db
 mkdir -p db/Multi
-cp "$user"/MicroMsg.db.dec.db db/
-cp "$user"/Multi/MSG0.db.dec.db db/Multi/
+cp "$user"/MicroMsg.db db/
+cp "$user"/Multi/MSG0.db db/Multi/
 tar -czvf archive-$(date +%F).tar.gz db
 if [ -f ".upload.sh" ]; then
     bash .upload.sh
