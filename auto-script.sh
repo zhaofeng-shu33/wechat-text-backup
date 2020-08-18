e88f87dd35b9df380052e5ff3b5fddbca3c32461
zhaofeng-shu33 <616545598@qq.com>
2020-08-18 11:47:08 +0800

    full path

--- auto-script.sh
+++ auto-script.sh
@@ -4,4 +4,4 @@ set -e -x
 ssh feng@10.1.1.4 "cd source\repos\wechat-text-backup && \"C:\Program Files\Git\bin\bash.exe\" win-upload-script.sh"
 bash linux-process-script.sh
 python3 extract.py
-ossutil64 cp -rf read oss://freiwilliger/wechat/
+/usr/local/bin/ossutil64 cp -rf read oss://freiwilliger/wechat/
