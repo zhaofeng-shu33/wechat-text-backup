b0fd8ecee77e49bb265dcc6ca07402e4a65c9ae2
zhaofeng-shu33 <616545598@qq.com>
2021-07-17 15:51:52 +0800

    add msg2

--- extract.py
+++ extract.py
@@ -139,7 +139,7 @@ def get_chatroom_list(cursor):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--filename', default=['Multi/MSG0.db.dec.db', 'Multi/MSG1.db.dec.db'], nargs='+')
+    parser.add_argument('--filename', default=['Multi/MSG0.db.dec.db', 'Multi/MSG1.db.dec.db', 'Multi/MSG2.db.dec.db'], nargs='+')
     parser.add_argument('--micro_filename', default='MicroMsg.db.dec.db')
     parser.add_argument('--enable_images', const=True, default=False, nargs='?')
     parser.add_argument('--enable_links', const=True, default=False, nargs='?')
--- win-upload-script.sh
+++ win-upload-script.sh
@@ -5,6 +5,7 @@ mkdir -p db/Multi
 cp "$user"/MicroMsg.db db/
 cp "$user"/Multi/MSG0.db db/Multi/MSG0.db
 cp "$user"/Multi/MSG1.db db/Multi/MSG1.db
+cp "$user"/Multi/MSG2.db db/Multi/MSG2.db
 tar -czvf archive-$(date +%F).tar.gz db
 if [ -f ".upload.sh" ]; then
     bash .upload.sh
