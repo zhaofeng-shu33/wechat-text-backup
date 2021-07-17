1b5702a03c2ddfb2b6df6873b3957a7dffb60d58
zhaofeng-shu33 <616545598@qq.com>
2021-07-17 16:03:58 +0800

    add count MSG auto

--- win-upload-script.sh
+++ win-upload-script.sh
@@ -3,9 +3,12 @@ user=~/Documents/"WeChat Files"/zhaofeng_shu33/Msg
 rm -rf db
 mkdir -p db/Multi
 cp "$user"/MicroMsg.db db/
-cp "$user"/Multi/MSG0.db db/Multi/MSG0.db
-cp "$user"/Multi/MSG1.db db/Multi/MSG1.db
-cp "$user"/Multi/MSG2.db db/Multi/MSG2.db
+
+mapfile -t db_file_list < <(ls Multi | grep ^MSG[0-9].db$)
+for i in "${db_file_list[@]}"; do
+    cp "$user"/Multi/$i db/Multi/$i
+done
+
 tar -czvf archive-$(date +%F).tar.gz db
 if [ -f ".upload.sh" ]; then
     bash .upload.sh
